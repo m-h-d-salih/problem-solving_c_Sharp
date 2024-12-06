141b22a01afb17a2d42e0c6089a15ea45fe256f8
m-h-d-salih <salihsha656@gmail.com>
2024-12-06 17:32:27 +0530

    leetcode 167

---
+++ leetCode-167/leetCode-167/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace leetCode_167
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 7, 11, 15 };
+            Console.WriteLine(string.Join(", ", TwoSum(arr, 9)));
+
+        }
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int left = 0, right = numbers.Length - 1;
+            while (left < right)
+            {
+                int sum = numbers[left] + numbers[right];
+                if (sum == target)
+                {
+                    return new int[] { left + 1, right + 1 }; ;
+                }
+                else if (sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            return Array.Empty<int>();
+            ;
+        }
+    }
+}
+
+       
+
+
---
+++ leetcode3289/leetcode3289/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode3289
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSneakyNumbers([0,1,1,0]));
+        }
+        public static int[] GetSneakyNumbers(int[] nums)
+        {
+            List<int> result = new List<int>(); 
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < nums.Length; j++) 
+                {
+                    if (nums[i] == nums[j] && i != j) 
+                    {
+                        count++;
+                    }
+                }
+                if (count == 1) 
+                {
+                    if (!result.Contains(nums[i])) 
+                    {
+                        result.Add(nums[i]);
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
