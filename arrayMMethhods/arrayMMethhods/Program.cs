9c32df3ea1d79cc9befcea1769ec8a3070695375
Mohammed Salih k <salihsha656@gmail.com>
2025-02-10 19:34:27 +0530

    final

---
+++ arrayMMethhods/arrayMMethhods/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace arrayMMethhods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 1, 2, 3, 4, 5, };
+            Array myArray=Array.CreateInstance(typeof(int),5);
+            foreach(int i in myArray)
+            {
+                Console.WriteLine(i);
+            }  
+        }
+    }
+}
