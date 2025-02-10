6be571c7627ac7afa1b9401ade637280acd43675
Mohammed Salih k <salihsha656@gmail.com>
2025-02-10 13:14:10 +0530

    ll

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -7,6 +7,10 @@
             Console.WriteLine("Hello, World!");
             Back.Hello("salih");
             Console.WriteLine("completed");
+            Myconstructor my = new Myconstructor("salih");
+            string greet=my.Message();
+            Myname name = new Myname("salih");
+            Console.WriteLine(name.Message());
         }
 
     }
@@ -15,6 +19,31 @@
         public static void Hello(string message)
         {
             Console.WriteLine(message);
+
+        }
+    }
+    public class Myconstructor
+    {
+        string  message;
+        public Myconstructor(string message)
+        {
+            this.message=message;
+        }
+        public string Message()
+        {
+            return "hello"+message;
+        }
+    }
+    public class Myname : Myconstructor
+    {
+        string name;
+        public Myname(string message) : base(message)
+        {
+            this.name=message;
+        }
+        public string Message()
+        {
+            return "hello  "+name;
         }
     }
 }
