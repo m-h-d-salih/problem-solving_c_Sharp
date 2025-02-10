76ae5fd5de36778e80b8a98d6e3e31d7776b652e
Mohammed Salih k <salihsha656@gmail.com>
2025-02-10 15:36:16 +0530

    arrat

---
+++ arrayEg/arrayEg/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace arrayEg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter size of the arra");
+            int n=int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for(int i = 0; i < a.Length; i++)
+            {
+                a[i]=int.Parse(Console.ReadLine());
+            }
+            for(int i = 0;i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);    
+            }
+            string[] b = { "bugatti", "pagani" };
+            foreach(string i  in b)
+            {
+                Console.WriteLine(i);
+            }
+            var s = b.Where(n => n == "bugatti");
+            Console.WriteLine(string.Join(", ",s));
+            var s1 = b.FirstOrDefault(n => n == "pagani");
+            Console.WriteLine(s1);
+        }
+    }
+}
