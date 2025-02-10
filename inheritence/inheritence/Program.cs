f6c652fae5201a035dd26492573aa85d1f62214b
Mohammed Salih k <salihsha656@gmail.com>
2025-02-10 14:50:49 +0530

    interface

---
+++ inheritence/inheritence/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace inheritence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Calculate    calculate = new Calculate();
+            Console.WriteLine(calculate.sum(10));
+            calculate.Hello();
+        }
+    }
+    public interface Ihello
+    {
+        void Hello();
+        int sum(int x);
+    }
+    public class Calculate:Ihello
+    {
+        public int sum(int x)
+        {
+            return x * 2;
+        }
+        public void Hello()
+        {
+            Console.WriteLine("COMPLETED");
+        }
+    }
+}
