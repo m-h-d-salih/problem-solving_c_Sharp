c741c4dd66508924b0290b7a62a64b8ee34401ab
Mohammed Salih k <salihsha656@gmail.com>
2025-02-10 10:47:51 +0530

    h

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Back.Hello("salih");
+            Console.WriteLine("completed");
+        }
+
+    }
+    public static class Back
+    {
+        public static void Hello(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
