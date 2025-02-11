db3dfe9a288445c48cb636be40edf7d26add808d
Mohammed Salih k <salihsha656@gmail.com>
2025-02-11 14:17:10 +0530

    list

---
+++ lissteg/lissteg/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace lissteg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            List<int> list1 = new List<int>();
+            list.Add("salih");
+            list.Add("fasal");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            list.Remove("fasal");
+            Console.WriteLine(String.Join(", ", list));
+            for(int i = 1; i < 3; i++)
+            {
+                list1.Add(i);
+            }
+            Console.WriteLine(list1.Contains(2));
+            list.Union(list);
+            Console.WriteLine(String.Join(',',list));
+
+            
+        }
+    }
+}
