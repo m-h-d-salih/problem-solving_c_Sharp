e448ccdbc0b67dd99245a1bb79f72d5a0d0fe505
Mohammed Salih k <salihsha656@gmail.com>
2025-02-11 12:16:02 +0530

    goto

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Emit;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+            bool b=false;
+            if(b)
+            goto label1;
+            else goto label2;
+            label1:
+            Console.WriteLine("label 1");
+            label2:
+            Console.WriteLine("label 2");
+            Console.WriteLine("end");
+        }
+    }
+}
