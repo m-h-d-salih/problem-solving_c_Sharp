309080b9f34753dd217b358b7bc49fddfbf68fd9
Mohammed Salih k <salihsha656@gmail.com>
2025-02-11 14:20:58 +0530

    hashset

---
+++ hashset/hashset/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace hashset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           HashSet<string> set = new HashSet<string>() { "salih","salih","fasal"};
+            Console.WriteLine(String.Join(", ",set));
+            set.Add("priya");
+            Console.WriteLine(String.Join(", ", set));
+            set.Clear();
+            Console.WriteLine(String.Join(", ", set)+" empty");
+
+        }
+    }
+}
