19fa0a18b61bb271f5801de0bc585ed4d41de647
Mohammed Salih k <salihsha656@gmail.com>
2025-02-11 13:36:28 +0530

    numeric sort

---
+++ arraymethods2wd/arraymethods2wd/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace arraymethods2wd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr= { 11, 2, 13 ,4};
+            foreach (int i in arr) { 
+            Console.WriteLine(i);
+            }
+            Array.Sort(arr);
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
