9a27255da791c2460a31cc07440afebebe7b07a9
Mohammed Salih k <salihsha656@gmail.com>
2025-02-11 11:39:52 +0530

    literals

--- arrayMMethhods/arrayMMethhods/Program.cs
+++ arrayMMethhods/arrayMMethhods/Program.cs
@@ -10,7 +10,31 @@
             foreach(int i in myArray)
             {
                 Console.WriteLine(i);
-            }  
+            }
+
+            string s = "hello";
+            s = "h";
+            Console.WriteLine(s);
+            Console.Beep();
+            int dec = 10;
+            int bin=0b10;
+            int hexx = 0X12f;
+            Console.WriteLine($"decimal = {dec} binary = {bin} hexadecimal = {hexx}");
+            double a = 10.15; //By Default Floating Point Literal is double
+            //Float Literal
+            float b = 100.72F; //Suffix with F
+            //Double Literal
+            double c = 1.45D; //Suffix with D
+            //Decimal Literal
+            decimal d = 1.44M; //Suffix with M
+
+            Console.WriteLine($"Double Literal: {a}");
+            Console.WriteLine($"Float Literal: {b}");
+            Console.WriteLine($"Double Literal: {c}");
+            Console.WriteLine($"Decimal Literal: {d}");
+            Console.WriteLine("hloo \n how are \t u");
+            Console.WriteLine(@"helllo \n how are \t u");
+            Console.ReadKey();
         }
     }
 }
