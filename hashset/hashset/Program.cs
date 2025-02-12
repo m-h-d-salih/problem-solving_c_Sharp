896d330c6bf5a641b9b3e2f0ab9dfe2e1d36121f
Mohammed Salih k <salihsha656@gmail.com>
2025-02-12 15:12:40 +0530

    linq

--- hashset/hashset/Program.cs
+++ hashset/hashset/Program.cs
@@ -5,12 +5,20 @@
         static void Main(string[] args)
         {
            HashSet<string> set = new HashSet<string>() { "salih","salih","fasal"};
+            List<int> list = new List<int>() { 1,2};
             Console.WriteLine(String.Join(", ",set));
             set.Add("priya");
             Console.WriteLine(String.Join(", ", set));
-            set.Clear();
+            
             Console.WriteLine(String.Join(", ", set)+" empty");
-
+            Dictionary<int,string> d=new Dictionary<int, string>();
+            for(int i = 0; i < set.Count; i++)
+            {
+                d.Add(list[i], set.ElementAt(i));
+            }
+            Console.WriteLine(String.Join(", ",d));
+            set.Clear();
         }
     }
 }
+ 
---
+++ linqeg4/linqeg4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace linqeg4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1,2,3,4,5,6,7};
+            var res=list.Where(x=>x%2==0);
+
+            Console.WriteLine(String.Join(", ",res));
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            //query synntax
+            var a = from n in numbers
+                    where n % 2 == 0
+                    select n;
+            Console.WriteLine(String.Join(", ", a));
+            int[] arr = { 1, 2, 3,4,5};
+            list.Clear();
+            foreach(int x in arr)
+            {
+                if(x%2==0)
+                list.Add(x);
+            }
+            var sorted= list.OrderByDescending(x=>x);
+            Console.WriteLine(String.Join(", ",sorted));
+        }
+    }
+}
